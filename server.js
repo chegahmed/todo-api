d983dd6f56a18a3dedaff0d723abc0d4ef58e9bd
Ahmed chega <chegahemd@gmail.com>
2017-03-14 14:55:45 +0000

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -5,30 +5,54 @@
 var express =require('express');
 var app =express();
 var PORT =process.env.PORT || 3000;
-//var middleware =require('./middleware');
+var todos = [{
+    id:1,
+    description: 'Meet nom for lunch',
+    completed: false
+},{
+    id:2,
+    description: 'Go to market',
+    completed: false
+},{
+    id:3,
+    description: 'Feed the cat',
+    completed: true
+}];
 
 app.get('/',function (req,res) {
     res.send('Todo API Root!');
-})
+});
 
+//GET /todos
+app.get('/todos',function (req,res) {
+    res.json(todos);
+});
+
+//GET /todos/:id
+app.get('/todos/:id',function (req,res) {
+    var todoId =parseInt(req.params.id , 10);
+    var matchedTodo;
+
+    todos.forEach(function (todo) {
+        if(todoId === todo.id){
+            matchedTodo = todo;
+        }
+    });
+
+    if(matchedTodo){
+        res.json(matchedTodo);
+    }else{
+        res.status(404).send();
+    }
+
+    res.send('Asking for todo with id of '+ req.params.id);
+});
 
 app.listen(PORT,function () {
     console.log('Express listening on port '+PORT +' !');
 });
 
 
-/*
-
- //app.use(middleware.requireAuthentication);
- app.use(middleware.logger);
-
- app.get('/about',middleware.requireAuthentication,function (req,res) {
- res.send('About As!');
- });
-
- app.use(express.static(__dirname+'/public'));
- //console.log(__dirname)
- */
 
 
 
