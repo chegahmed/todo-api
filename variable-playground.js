47dc91ad45a40f4c56c9ec4a1c3a47fcfe6ab33e
Ahmed chega <chegahemd@gmail.com>
2017-03-15 16:43:33 +0000

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+/*
+/!**
+ * Created by ahmed on 15/03/2017.
+ *!/
+
+var person = {
+    name: 'Ahmed',
+    age:22
+};
+
+function updatePerson(obj) {
+/!*    obj = {
+        name:'Ahmed',
+        age:24
+    };*!/
+    obj.age =25;
+}
+
+updatePerson(person);
+console.log(person);*/
+var grades =[15,88];
+function addGrades(gradesArr , newGrade) {
+gradesArr.push(55);
+    debugger;
+
+//gradesArr = [12,33,99];
+}
+
+addGrades(grades);
+console.log(grades);
