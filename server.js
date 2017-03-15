3de693b86d9eaa74eba11ac90963240bd27662b1
Ahmed chega <chegahemd@gmail.com>
2017-03-15 12:31:39 +0000

    Refactor API with underscore

--- server.js
+++ server.js
@@ -70,6 +70,31 @@ app.delete('/todos/:id',function (req,res) {
     }
 });
 
+//PUT  /todos/:id
+app.put('/todos/:id',function (req,res) {
+    var body = _.pick(req.body,'description','completed') ;//use _.pick to only pick description znd completed
+    var validAttributes ={};
+
+    if(body.hasOwnProperty('completed')&&_.isBoolean(body.completed)){
+        validAttributes.completed =body.completed;
+    }else if(body.hasOwnProperty('completed')){
+        return res.status(400).send();
+    }
+
+    if(body.hasOwnProperty('description')&&_.isString(body.description)&&body.description.trim()){
+        validAttributes.description =body.description;
+    }else if(body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }
+
+
+    //HERE 
+
+
+});
+
+
+
 app.listen(PORT,function () {
     console.log('Express listening on port '+PORT +' !');
 });
